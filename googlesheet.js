a55078053bb893a3db8430ae855806628ab168f6
rajukr9824 <130588040+rajukr9824@users.noreply.github.com>
2023-11-18 16:36:57 +0530

    portfolio final submit

---
+++ googlesheet.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwGkuOlaUcjG7enI5gQ2FpcRtIy9bjjjtNGbwtcTSLnR2qhvYCdM3Cbm4AlyZUNRxWE8A/exec'
+const form = document.forms['contact-form']
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("Thank you! your form is submitted successfully."))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+})
